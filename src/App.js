d1a51854c672bd1649632148010851ee9b779b63
daniabajo <dani.abajo@gmail.com>
2023-11-05 13:13:25 +0100

    Web

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 import Component from './Component';
 
 function App() {
   return (
     <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
       <header className="App-header">
         <p>
-          PRÓXIMAMENTE...
+          próximamente...
         </p>
       </header>
     </div>
