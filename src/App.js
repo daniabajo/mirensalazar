8bd34e0fdb69648fe7e3ef7da3ffd215060601b6
daniabajo <dani.abajo@gmail.com>
2023-11-05 12:26:28 +0100

    Web

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Anna Vehi
+          PRÓXIMAMENTE...
         </p>
       </header>
     </div>
