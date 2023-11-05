827d50e8b90417c68e7be14b4cb913c8bb76b710
daniabajo <dani.abajo@gmail.com>
2023-11-05 14:19:51 +0100

    Hola

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <img src={logo} className="App-logo" alt="logo" />
       <header className="App-header">
         <p>
-          próximamente...
+          PRÓXIMAMENTE...
         </p>
       </header>
     </div>
