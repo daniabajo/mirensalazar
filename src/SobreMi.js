c2ba68bbca49c9172d256c4e5c80df851100da37
DaniAbajo <31502192+daniabajo@users.noreply.github.com>
2023-11-26 19:41:01 +0100

    Delete src/SobreMi.js

--- src/SobreMi.js
+++
@@ -1,8 +0,0 @@
-
-export default function sobremi() {
-    return (
-        <div className="AppClass">
-            <h className="HeaderClass">  <p> Sobre mí en construcción...</p> </h>
-        </div>
-    )
-}
