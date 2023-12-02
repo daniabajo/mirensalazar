b5b25ca0da5e3797aed5012c4b180eb4fa5bdc53
daniabajo <dani.abajo@gmail.com>
2023-12-02 14:32:43 +0100

    h

--- src/App.js
+++ src/App.js
@@ -3,48 +3,31 @@ import './App.css';
 import Logo from './Logo.png';
 import Navbar from './Navbar';
 import Homes from './pages/Home';
-import Sobremi from './sobremi';
+import Sobremi from './pages/sobremi';
 import Servicios from './pages/Servicios';
 import Reserva from './pages/Reserva';
 import Contacto from './pages/Contacto';
 //import Map from './Map';
 import credentials from './credentials';
-
-
+import { Route, Routes } from "react-router-dom";
 const mapURL = `https:/maps.googleapis.com/maps/api/js?v=3.exp&key=${credentials.mapsKey}`;
-
 function App() {
-  let Page
-  switch (window.location.pathname) {
-    case "/": Page = <Homes />
-      break
-    case "/home": Page = <Homes />
-      break
-    case "/sobremi": Page = <Sobremi />
-      break
-    case "/servicios": Page = <Servicios />
-      break
-    case "/contacto": Page = <Contacto />
-      break
-    case "/reservacita": Page = <Reserva />
-      break
-    default:
-        Page = <Contacto />
-  }
-
   return (
-    <div className="AppClass">
+    <>
+      <div className="AppClass">
       <img src={Logo} className="LogoClassMain" alt="logo" />
       <Navbar />
-      {Page}
-      {/*<Map  
-      //googleMapURL= {mapURL}
-      //containerElement={<div style={{heigth:'400px'}}/>}
-      //mapElement={<div style={{heigth:'100%'}}/>}
-      //loadingElement={<p> Cargando </p>}
-      ///>*/}
-    </div>
+        <Routes>
+          <Route path="/" element={<Homes />} />
+          <Route path="/sobremi" element={<Sobremi />} />
+          <Route path="/servicios" element={<Servicios />} />
+          <Route path="/contacto" element={<Contacto />} />
+          <Route path="/reservacita" element={<Reserva />} />
+        </Routes>
+      </div>
+
+    </>
+
   );
 }
-
 export default App;
