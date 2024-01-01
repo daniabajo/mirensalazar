da89f051d2379a0ca20b2e3246975f7b7c2a0a67
AnnaVehi <vehi.vidal@gmail.com>
2024-01-01 13:07:17 +0100

    w

--- src/pages/sobremi.js
+++ src/pages/sobremi.js
@@ -8,38 +8,81 @@ export default function Sobremi() {
             <img src={Consulta} className="ImageTransversal" alt="Consulta" />
             <div className='SobremiBase'>
             <div className='SobremiFilofia'>
-                <h className="TituloDorado">  <a> Filosofía</a> </h>
+                <h className="TituloDorado">  <a>¡Bienvenidos! </a> </h>
                 <div className='SobremiFilofiaFila'>
-                    <div className='SobremiFilofiaTexto'> 
-                        <a>En nuestro centro de nutrición, abrazamos una filosofía centrada en el bienestar integral, reconociendo que la alimentación no solo es un acto físico, sino también un componente esencial para la salud mental y emocional. Creemos en la individualidad de cada persona y en la importancia de diseñar programas nutricionales personalizados, adaptados a sus necesidades y metas específicas. Nuestra filosofía se fundamenta en el poder transformador de una alimentación equilibrada y sostenible, promoviendo hábitos saludables que perduren a lo largo del tiempo. </a>
-                        <p>Valoramos la educación nutricional como una herramienta fundamental para capacitar a nuestros clientes, permitiéndoles tomar decisiones informadas sobre su salud. Fomentamos la conexión con los alimentos, inspirando el disfrute consciente de cada comida. En nuestro centro, abogamos por la prevención de enfermedades a través de la nutrición y trabajamos en colaboración con nuestros clientes para superar desafíos, establecer objetivos realistas y lograr un cambio positivo y duradero en sus vidas. Nuestra filosofía se basa en el respeto por la diversidad alimentaria, la inclusión y el apoyo continuo, creando un ambiente que nutre no solo el cuerpo, sino también el espíritu de quienes confían en nosotros para guiarlos hacia una salud óptima.</p>
+                    <div className='SobremiBienvenidosTexto'> 
+                        <a>Soy Anna Vehí y me apasiona ayudar a las personas a mejorar su salud y calidad de vida, guiándoles y acompañándoles en la consecución de sus distintos objetivos nutricionales y de hábitos saludables. </a>
                     </div>
                     <img className='SobremiFilosfiaFoto' src={LeadMagnet} alt="LeadMagnet" />
                 </div>
             </div>
             <div className='SobremiHistoria'>
                 <h className="TituloDorado">  <a> Mi historia</a> </h>
-                <p>Anna, una joven apasionada por la salud y el bienestar, decidió seguir su sueño de convertirse en nutricionista desde una edad temprana. Después de completar su licenciatura en Nutrición y Dietética, Anna se sumergió en un programa de internado dietético, donde adquirió experiencia práctica en entornos clínicos y comunitarios. Durante este período, se especializó en nutrición deportiva, fascinada por la conexión entre la alimentación y el rendimiento físico.</p>
-                <p>Después de obtener su certificación como Nutricionista Registrada, Anna comenzó a trabajar en un hospital, brindando asesoramiento nutricional a pacientes con diversas condiciones de salud. Su enfoque compasivo y su habilidad para comunicar conceptos nutricionales complejos la destacaron entre sus colegas. A medida que su carrera progresaba, Anna decidió abrir su propia consulta privada, donde combina su experiencia clínica con un enfoque personalizado, ayudando a sus clientes a alcanzar sus metas de salud y bienestar a través de una alimentación equilibrada. La historia de Anna refleja su dedicación a la profesión y su deseo continuo de impactar positivamente en la vida de los demás a través de la nutrición. </p>
+                <p>Hija de médico y nieta y sobrina de farmacéuticos, casi se podría decir que las ciencias de la salud forman parte de mi ADN. Trabajando como farmacéutica en Barcelona y en el Reino Unido, me di cuenta de que mi respuesta natural ante el paciente consistía en indagar acerca de sus hábitos de vida para descubrir si alguna modificación de éstos pudiera mejorar sus dolencias de forma más permanente. La voluntad de querer proporcionar una educación nutricional más extensa y detallada a los pacientes me llevó a estudiar el Grado en Nutrición Humana y Dietética, aunque el cometido de transmitir mis conocimientos a la población era difícil de alcanzar en un contexto de farmacia comunitaria, dado que desafortunadamente no se suele gozar de un tiempo de calidad con los pacientes. Es por este motivo que finalmente decidí emprender mi propio proyecto, y a día de hoy me emociona enormemente poder realizar mi propósito en mi centro de nutrición.</p>
+            </div>
+            <div className='SobremiHistoria'>
+                <h className="TituloDorado">  <a> Mi método</a> </h>
+                <p>Como farmacéutica y dietista-nutricionista, ambas siendo profesiones sanitarias, entiendo que el pilar más importante alrededor del cual toda intervención nutricional debe gravitar es la promoción de la salud. Por este motivo, mi método de trabajo se basa en:</p>
+                <ul>
+                    <li>el consumo de <strong>alimentos</strong>, sin hacer uso de productos artificiales</li>
+                    <li>una educación alimentaria que capacite al paciente para poder entender sus necesidades y llegar a ser autónomo</li>
+                    <li>un cambio de hábitos progresivo y permanente, evitando así el efecto rebote y garantizando unos resultados que se mantienen a largo plazo</li>
+                    <li>evitar sensaciones nocivas que suelen ser contraproducentes, tales como la restricción, el esfuerzo, el sufrimiento o pasar hambre</li>
+                    <li>un alto grado de individualización, dado que todos somos diferentes y cada individuo se beneficiará de unas herramientas o estrategias distintas</li>
+                    <li>un ambiente agradable, distendido, empático y de confianza donde trabajamos conjuntamente con el paciente para ayudarle en la consecución de todos sus objetivos</li>
+                    <li>el compromiso de mi formación continua para poder proporcionar una atención nutricional cada vez mejor</li>
+                </ul>
             </div>
             <div className='SobremiTrayectoria'>
-                <h className="TituloDorado">  <a> Trayectoria profesional</a> </h>
+                <h className="TituloDorado">  <a> Formación</a> </h>
+                <div className="SobremiDivisionTexto"> <a className="SobremiTextoMayus"> GRADOS UNIVERSITARIOS </a> </div>
                 <ul>
-                    <li>Obtener un título universitario en nutrición, dietética, ciencias de los alimentos o un campo relacionado. Muchos nutricionistas tienen al menos una licenciatura en nutrición.</li>
-                    <li>Realizar pasantías o prácticas clínicas supervisadas para obtener experiencia práctica en entornos de atención médica.</li>
-                    <li>En algunos lugares, obtener una licencia o certificación es necesario para ejercer como nutricionista. Asegúrate de cumplir con los requisitos locales y obtener las licencias necesarias.</li>
-                    <li>Algunos nutricionistas eligen especializarse en áreas específicas como nutrición deportiva, nutrición clínica, nutrición pediátrica o nutrición comunitaria. Obtener certificaciones adicionales puede ser beneficioso.</li>
-                    <li>Algunos nutricionistas optan por obtener una maestría para avanzar en sus conocimientos y habilidades. La educación continua a través de conferencias, seminarios y cursos también es fundamental para mantenerse actualizado en el campo.</li>
-                    <li>Desarrollar habilidades de comunicación efectiva y empatía para trabajar de manera efectiva con los clientes y colegas de la industria de la salud.</li>
-                    <li>Obtener la certificación como Nutricionista Registrado (Registered Dietitian, RD) en algunos países, como en los Estados Unidos. Esto a menudo implica completar un programa de internado dietético y aprobar un examen.</li>
-                    <li>Ganar experiencia práctica trabajando en entornos de atención médica, hospitales, clínicas, consultorios privados o instituciones de investigación.</li>
-                    <li>Construir una red profesional participando en conferencias, eventos de la industria y conectándose con otros profesionales de la salud.</li>
-                    <li>Unirse a asociaciones profesionales, como la Academia de Nutrición y Dietética, para mantenerse conectado con la comunidad de nutricionistas y acceder a recursos actualizados.</li>
-                    <li>Participar en programas de educación continua para estar al tanto de las últimas investigaciones y avances en nutrición y dietética.</li>
-                    <li>Algunos nutricionistas optan por establecer sus propias prácticas privadas, ofreciendo servicios de asesoramiento nutricional a clientes individuales o grupos.</li>
-
-
+                    <li>Grado en Farmacia (Universidad de Barcelona)</li>
+                    <ul>
+                        <li>Premio extraordinario de grado - mejor expediente académico de la Facultad de Farmacia</li>
+                        <li>Erasmus: University College London (Reino Unido) adherida al grupo de investigación Drug Discovery and Therapeutic Target Identification Research Cluster</li>
+                        <li>Erasmus: Royal National Orthopaedic Hospital (Reino Unido) adherida al Departamento de Farmacia</li>
+                    </ul>
+                    <li>Grado en Nutrición Humana y Dietética (Universidad Europea de Madrid)</li>
+                    <ul>
+                        <li>Prácticas curriculares: consulta de nutrición de la Farmacia Les Arenes</li>
+                        <li>Prácticas curriculares: nutrición deportiva en Club de Tennis Sant Gervasi</li>        
+                        <li>Prácticas curriculares: nutrición deportiva en CAR Sant Cugat</li>
+                        <li>Prácticas curriculares: consulta de nutrición de Neus Elcacho</li>
+                    </ul>        
                 </ul>
+                <div className="SobremiDivisionTexto"> <a className="SobremiTextoMayus"> ACREDITACIONES </a> </div>
+                <ul>
+                    <li>Antropometrista ISAK nivel 1</li>
+                </ul>
+                <div className="SobremiDivisionTexto"> <a className="SobremiTextoMayus"> POSTGRADOS </a> </div>
+                <ul>
+                    <li>Entrevista motivacional en la práctica clínico-nutricional (Academia Española de Nutrición y Dietética)</li>
+                    <li>Curso avanzado en nutrición para la optimización de la composición corporal en deportistas y población general (Academia Española de Nutrición y Dietética)</li>
+                    <li>Ayudas ergogénicas en el ámbito deportivo y de salud (Academia Española de Nutrición y Dietética)</li>
+                    <li>Vegetarianismo y planificación dietética en alimentación vegetariana (Universitat de les Illes Balears)</li>
+                </ul>
+                <div className="SobremiDivisionTexto"> <a className="SobremiTextoMayus"> CURSOS </a> </div>
+                <ul>
+                    <li>Binomio sueño y alimentación: una nueva herramienta en la práctica profesional del dietista-nutricionista (Academia Española de Nutrición y Dietética)</li>
+                    <li>I Workshop sobre obesidad y nutrición: una visión integral de la obesidad más allá de la báscula (Academia Española de Nutrición y Dietética)</li>
+                    <li>I Workshop sobre microbiota, probióticos y prebióticos (Academia Española de Nutrición y Dietética)</li>
+                    <li>Toma ConCiencia (Academia Española de Nutrición y Dietética)</li>
+                    <li>Abordaje dietético en la enfermedad cardiovascular: nuevas evidencias (Academia Española de Nutrición y Dietética)</li>
+                    <li>Abordaje de la hipertensión arterial en la farmacia comunitaria (Consell de Col·legis Farmacèutics de Catalunya)</li>
+                    <li>Actividad física y salud - intervención en la atención primaria (Societat Catalana de Medicina Familiar i Comunitària) </li>
+                    <li>Intervención en tabaquismo (Societat Catalana de Medicina Familiar i Comunitària) </li>
+                    <li>Soporte nutricional al paciente oncológico (Associació Contra el Càncer & Codinucat)</li>
+                    <li>Bioimpedancia: una tecnología clave para valorar la salud y el rendimiento deportivo (INEFC)</li>
+                </ul>
+                <div className="SobremiDivisionTexto"> <a className="SobremiTextoMayus"> CONGRESOS </a> </div>
+                <ul>
+                    <li>XXVI Jornadas Internacionales de Nutrición Práctica</li>
+                    <li>XV Congreso Internacional de SEDCA</li>
+                    <li>I Nutrition Week (Universitat de les Illes Balears)</li>
+                    <li>I Congreso de personas que viven con obesidad (Asociación Bariátrica Híspalis)</li>
+                </ul>
+
             </div>
         </div>
         </div>
