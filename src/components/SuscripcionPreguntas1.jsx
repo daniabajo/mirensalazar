a467a3f4c66f4525520c45ece86739e6bfd8070d
daniabajo <dani.abajo@gmail.com>
2024-01-02 10:54:54 +0100

    h

---
+++ src/components/SuscripcionPreguntas1.jsx
@@ -0,0 +1,13 @@
+import './SuscripcionPreguntas1.css';
+
+const SuscripcionPreguntas1 = () => {
+    return (
+        <div>
+            <div className="PreguntasContainer">
+                <a className="TextoBasicoEncabezadoBeige">¿Tienes dudas sobre nuestro sistema de suscripciones?</a>
+                <a className="TextoBasicoEncabezadoBeige">Consulta la sección <strong> Preguntas frecuentes </strong> más abajo o ponte en contacto con nosotros y te ayudaremos a resolverlas.</a>
+            </div>
+        </div>
+    )
+}
+export default SuscripcionPreguntas1;
